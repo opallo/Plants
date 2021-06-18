09f67c4615adf91b3232990a617066423be1ec93
ynglarg <opallo.collins@gmail.com>
2021-06-18 02:57:42 -0700

    Started on tooltip system

---
+++ plants/Assets/Scripts/TooltipTrigger.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+  public void OnPointerEnter(PointerEventData eventData) {
+    TooltipSystem.Show();
+  }
+  public void OnPointerExit(PointerEventData eventData) {
+    TooltipSystem.Hide();
+  }
+}
